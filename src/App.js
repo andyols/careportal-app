4bd257322b0b3737b440a53285bb4d7a017a25d3
Andy Olson <andyolson09@gmail.com>
2019-05-04 13:25:48 -0500

    initial routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container, Header } from 'semantic-ui-react'
+import Landing from './components/landing/Landing'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Dashboard from './components/dashboard/Dashboard'
+import Contributions from './components/contributions/Contributions'
+import Profile from './components/profile/Profile'
+
 function App() {
   return (
     <Container>
       <Header textAlign="center">CarePortal App</Header>
+      <Router>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/contributions" component={Contributions} />
+        <Route exact path="/profile" component={Profile} />
+      </Router>
     </Container>
   )
 }
