56add6a60f60a26a45ae27b90bcd2b35c3b5f70f
Andy Olson <andyolson09@gmail.com>
2019-05-04 12:18:06 -0500

    clean react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container, Header } from 'semantic-ui-react'
+function App() {
+  return (
+    <Container>
+      <Header textAlign="center">CarePortal App</Header>
+    </Container>
+  )
+}
+
+export default App
