e1139efd52401e23a1e4d6e34f0bfe51c2eeab29
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-05-04 14:30:49 -0500

    first mac probs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container, Header } from 'semantic-ui-react'
-import Landing from './components/landing/Landing'
-import Login from './components/auth/Login'
-import Signup from './components/auth/Signup'
-import Dashboard from './components/dashboard/Dashboard'
-import Contributions from './components/contributions/Contributions'
-import Profile from './components/profile/Profile'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container, Sidebar } from "semantic-ui-react";
+import Landing from "./components/landing/Landing";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Dashboard from "./components/dashboard/Dashboard";
+import Contributions from "./components/contributions/Contributions";
+import Profile from "./components/profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Container>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/contributions" component={Contributions} />
-        <Route exact path="/profile" component={Profile} />
-      </Container>
+      <Sidebar.Pushable>
+        <Sidebar.Pusher>
+          <Container>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/contributions" component={Contributions} />
+            <Route exact path="/profile" component={Profile} />
+          </Container>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,66 +1,89 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { Menu } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Menu,
+  Sidebar,
+  Segment,
+  Button,
+  Image,
+  Header
+} from "semantic-ui-react";
 
-export default class MenuExampleContentProp extends Component {
-  state = {}
+export default class Navbar extends Component {
+  state = { visible: false };
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleHideClick = () => this.setState({ visible: false });
+  handleShowClick = () => this.setState({ visible: true });
+  handleSidebarHide = () => this.setState({ visible: false });
 
   render() {
-    const { activeItem } = this.state
+    const { visible } = this.state;
 
     return (
-      <Menu>
-        <Menu.Item
-          as={Link}
-          to="/"
-          name="landing"
-          active={activeItem === 'landing'}
-          content="Landing"
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          as={Link}
-          to="/dashboard"
-          name="dashboard"
-          active={activeItem === 'dashboard'}
-          content="Dashboard"
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          as={Link}
-          to="/login"
-          name="login"
-          active={activeItem === 'login'}
-          content="Login"
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          as={Link}
-          to="/signup"
-          name="signup"
-          active={activeItem === 'signup'}
-          content="Signup"
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          as={Link}
-          to="/contributions"
-          name="contributions"
-          active={activeItem === 'contributions'}
-          content="Contributions"
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          as={Link}
-          to="/profile"
-          name="profile"
-          active={activeItem === 'profile'}
-          content="Profile"
-          onClick={this.handleItemClick}
-        />
-      </Menu>
-    )
+      <>
+        <Button.Group>
+          <Button
+            disabled={visible}
+            onClick={this.handleShowClick}
+            icon="bars"
+            labelPosition="right"
+          />
+        </Button.Group>
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          icon="labeled"
+          inverted
+          onHide={this.handleSidebarHide}
+          vertical
+          visible={visible}
+          width="thin"
+        >
+          <Menu.Item as={Button} icon="times" />
+          <Menu.Item
+            as={Link}
+            to="/"
+            name="landing"
+            content="Landing"
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/dashboard"
+            name="dashboard"
+            content="Dashboard"
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/login"
+            name="login"
+            content="Login"
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/signup"
+            name="signup"
+            content="Signup"
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/contributions"
+            name="contributions"
+            content="Contributions"
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/profile"
+            name="profile"
+            content="Profile"
+            onClick={this.handleItemClick}
+          />
+        </Sidebar>
+      </>
+    );
   }
 }
